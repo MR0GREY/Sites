7b3362f22aca4b094bb9f3d7f6363784b0761662
Сергей <160958147+MR0GREY@users.noreply.github.com>
2025-10-20 10:25:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ const sidebar = document.querySelector('.sidebar');
 menuToggle.addEventListener('click', () => {
     sidebar.classList.toggle('hidden');
 });
+const audio = document.getElementById('audio');
+
+function playAudio() {
+  audio.play();
+}
+
+function pauseAudio() {
+  audio.pause();
+}
+
+function stopAudio() {
+  audio.pause();
+  audio.currentTime = 0;
+}
